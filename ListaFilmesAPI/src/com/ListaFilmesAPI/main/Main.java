eb72e268c142f21e8152fd6e2bd7880cb965e074
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-03-28 12:58:22 -0300

    Planejamento

--- ListaFilmesAPI/src/com/ListaFilmesAPI/main/Main.java
+++ ListaFilmesAPI/src/com/ListaFilmesAPI/main/Main.java
@@ -3,8 +3,12 @@ package com.ListaFilmesAPI.main;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// fazer conexão HTTP e buscar os top 250 filmes
+		
+		// extrair só os dados que interessam (titulo, poster, classificação)
+		
+		// exibir e manipular os dados
 	}
 
 }
