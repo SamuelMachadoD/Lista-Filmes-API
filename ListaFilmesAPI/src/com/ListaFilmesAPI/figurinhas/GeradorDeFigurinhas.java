8688ada30433dade8717c838c9bb95f0bf274589
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-03-28 17:11:19 -0300

    Leitura da imagem

--- ListaFilmesAPI/src/com/ListaFilmesAPI/figurinhas/GeradorDeFigurinhas.java
+++ ListaFilmesAPI/src/com/ListaFilmesAPI/figurinhas/GeradorDeFigurinhas.java
@@ -1,14 +1,22 @@
 package com.ListaFilmesAPI.figurinhas;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+
 public class GeradorDeFigurinhas {
 	
-	public void cria() {
+	public void cria() throws Exception {
 		
 		//leitura da imagem
-		
+		BufferedImage imagemOriginal = ImageIO.read(new File("entrada/filme.jpg"));
 		//cria nova imagem em memória com transparencia e com tamanho novo
 		
 		//copiar a imagem original para a nova em memória
+		
+		//escrever uma frase na nova imagem
+		
+		//escrever a nova imagem em um arquivo
 		
 		
 	}
