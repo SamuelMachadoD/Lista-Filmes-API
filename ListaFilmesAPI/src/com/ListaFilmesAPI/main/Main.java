fbb5b7f19850008ab1044b1cf51f8a671fe4002d
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-03-28 14:54:27 -0300

    Customização de texto

--- ListaFilmesAPI/src/com/ListaFilmesAPI/main/Main.java
+++ ListaFilmesAPI/src/com/ListaFilmesAPI/main/Main.java
@@ -14,7 +14,7 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 
 		// fazer conexão HTTP e buscar os top 250 filmes
-		String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json"; // Armazena o link da API
+		String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json"; // Armazena o link da API
 		URI endereco = URI.create(url); //Le e armazena o conteudo da API em uma url
 		var client = HttpClient.newHttpClient();//Instancia um cliente //é possivel trocar o HttpClient por var / deixando para o java sozinho definir como HttpClient ( é preciso esta especificado na direita )
 		var request = HttpRequest.newBuilder(endereco).GET().build();//Faz uma request com a URL //o mesmo do var pode ocorrer aqui
@@ -27,6 +27,14 @@ public class Main {
 		
 		
 		// exibir e manipular os dados
+		for (Map<String, String> filme : listaDeFilmes) { //passa por cada filme da listaDeFilmes
+			System.out.println("---------------------");
+			System.out.println("\u001b[1m\u001b[40m Rank: " + filme.get("rank") + " \u001b[m");
+			System.out.println("\u001b[1m\u001b[47m\u001b[30m " + filme.get("title") + " \u001b[m");
+			System.out.println(filme.get("image"));
+			System.out.println("\u001b[43m\u001b[1m Rating: " + filme.get("imDbRating") + " \u001b[m");
+			// escreve o que esta na chave desejada
+		}
 	}
 
 }
